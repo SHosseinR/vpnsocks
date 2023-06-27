1ef0737d0963806b936746c4e23e79bee3746e72
sh rahgoshay <hoseinshr1055@gmail.com>
2023-06-27 13:47:37 +0330

    while

--- server.sh
+++ server.sh
@@ -1,36 +1,39 @@
 #!/bin/sh
 
 
-host=`python3 api.py`
+while true
+do
+    host=`python3 api.py`
 
-echo ${host}
+    echo ${host}
 
-# host=$1
+    # host=$1
 
-#  -o PubkeyAcceptedKeyTypes=ssh-rsa 
-scp ./run.sh  $host:/root
-scp ./docker-compose.yml  $host:/root
+    #  -o PubkeyAcceptedKeyTypes=ssh-rsa 
+    scp ./run.sh  $host:/root
+    scp ./docker-compose.yml  $host:/root
 
-ssh $host 'apk add screen && screen -d -m bash run.sh' 
+    ssh $host 'apk add screen && screen -d -m bash run.sh' 
 
-# # ubuntu setting
-# PROXY_STRING="socks://localhost:1080/"
-# export socks_proxy=$PROXY_STRING
-# export SOCKS_PROXY=$PROXY_STRING
+    # # ubuntu setting
+    # PROXY_STRING="socks://localhost:1080/"
+    # export socks_proxy=$PROXY_STRING
+    # export SOCKS_PROXY=$PROXY_STRING
 
-# gsettings set org.gnome.system.proxy mode 'manual';
-# gsettings set org.gnome.system.proxy.socks host "'localhost'";
-# gsettings set org.gnome.system.proxy.socks port 8000;
+    # gsettings set org.gnome.system.proxy mode 'manual';
+    # gsettings set org.gnome.system.proxy.socks host "'localhost'";
+    # gsettings set org.gnome.system.proxy.socks port 8000;
 
-# apt_conf_proxy="
-# Acquire::socks::Proxy \"$PROXY_STRING\";
-# "
-# echo "$apt_conf_proxy" | sudo tee /etc/apt/apt.conf.d/proxy > /dev/null
+    # apt_conf_proxy="
+    # Acquire::socks::Proxy \"$PROXY_STRING\";
+    # "
+    # echo "$apt_conf_proxy" | sudo tee /etc/apt/apt.conf.d/proxy > /dev/null
 
-# echo "Proxy enabled."
-# ###
+    # echo "Proxy enabled."
+    # ###
 
 
-ssh -L 8000:localhost:1080  $host
+    ssh -L 8000:localhost:1080  $host
 
-# echo "" > /etc/apt/apt.conf.d/proxy 
+    # echo "" > /etc/apt/apt.conf.d/proxy 
+done
